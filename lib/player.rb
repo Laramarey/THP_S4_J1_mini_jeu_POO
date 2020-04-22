9442c229c4d2d04cbc466f5047497043b7288099
Laramarey <l.schutz@tbs-education.org>
2020-04-22 11:02:20 +0200

    ajout duend manquant pour que l'app2 tourne

--- lib/player.rb
+++ lib/player.rb
@@ -13,33 +13,35 @@ class Player
 
 
 def gets_damage(lost_points) #appliquer les pertes de vies"
-    @life_points -= lost_points
+  @life_points -= lost_points
     if @life_points < 1 #au lien de faire < ou = à 0 cela revient à faire strictement <1
       return  "Le joueur #{name} a été tué !"
     end
-end 
-      
+  end 
 
 def compute_damage #attribuer une perte aléatoire de vies
-    return rand(1..6)
-  end
+  return rand(1..6)
+end
 
 def attacks(player2) #lancement d'une attaque
   puts "#{@name} attaque #{player2.name} "
   damage = compute_damage
   puts "Il lui inflige #{damage} points de dommages !"
   if player2.life_points - damage < 1
-      puts player2.gets_damage(damage)
+    puts player2.gets_damage(damage)
   end 
   player2.life_points = player2.life_points - damage #condition pour que le calcul des points s'effecture correctement
-    return player2.show_state
-  end
+  return player2.show_state
+end
+end #attention à la fin de la classe
+
+
 
 class HumanPlayer < Player #ma deuxième classe
   attr_accessor :weapon_level#je veux ajouter un attribut weaper
 
   def initialize(name)#je rentre les mêmes variablesque pour la classe mère
-  super(name)
+    super(name)
     @life_points = 100 #j'utilise super pour que les changement ne s'appliquent qu'à la classe fille Human
     @weapon_level = 1
   end
@@ -53,7 +55,7 @@ class HumanPlayer < Player #ma deuxième classe
   def compute_damage #manouvelle méthode doit multiplier les dégâts
     super * @weapon_level #idem, je repars donc de la méthode de la classe mère grâce à super et je multiplie par weapon_level
   end #dégâts compris entre [@weapon_level] et [6 x @weapon_level]
-    
+
   def search_weapon
     puts "lance un dé en appuyant sur la touche entrée" 
     gets.chomp #pour laisser un intervalle d'intéraction avec le joueur
@@ -63,13 +65,13 @@ class HumanPlayer < Player #ma deuxième classe
         puts "Youhou ! Elle est meilleure qui ton arme actuelle : take the power!"
       else puts "Oupsi... Elle n'est pas mieux que ton arme actuelle, tu gardes donc l'actuelle..."
       end 
-  end 
+    end 
 
 
-  def search_health_pack
-    puts "lance un dé en appuyant sur la touche entrée" 
-    gets.chomp
-    pack_life = rand(1..6)
+    def search_health_pack
+      puts "lance un dé en appuyant sur la touche entrée" 
+      gets.chomp
+      pack_life = rand(1..6)
       if pack_life == 1 
         puts "Tu n'as rien trouvé..."
       elsif ( pack_life <= 5 ) 
@@ -79,12 +81,12 @@ class HumanPlayer < Player #ma deuxième classe
         puts "Waoww, tu as trouvé un pack de +80 points de vie!"
       end 
 
+
       if @life_points > 100
         @life_points = 100 
         else @life_points
       end 
 
     end 
-end
+  end
 
-end  
